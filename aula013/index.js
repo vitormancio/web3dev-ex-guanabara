9d050d1db85f5255ddbf47c415e4af942deb79ed
Vitor <vitor.mancio@ggmail.com>
2022-09-13 20:38:55 -0300

    up aula013

---
+++ aula013/index.js
@@ -0,0 +1,19 @@
+const button1 = document.querySelector('.button-1')
+const resultado = document.querySelector('.resultado')
+
+button1.addEventListener('click', () => {
+    const num1 = Number(prompt('Digite um valor:'))
+    const num2 = Number(prompt('Digite outro valor:'))
+
+    resultado.innerHTML = numeroMaior(num1,num2) ? `O ${num1} é maior que o ${num2}` : `O ${num2} é maior que o ${num1}`
+})
+
+function numeroMaior(num1,num2) {
+    if(num1 > num2) true
+
+    if(num2 > num1) false
+}
+
+
+
+
--- modelo/index.js
+++ modelo/index.js
@@ -1,4 +1,4 @@
-const button1 = document.querySelector('.buton-1')
+const button1 = document.querySelector('.button-1')
 const button2 = document.querySelector('.button-2')
 const resultado = document.querySelector('.resultado')
 
